41f0abc38b65edf686e69054d061e29b178ec31e
sarthak kanungo <skanungodgacm@gmail.com>
2019-07-23 21:04:00 +0530

    Update EmployeeController.java

--- src/main/java/com/springboot/project/controller/EmployeeController.java
+++ src/main/java/com/springboot/project/controller/EmployeeController.java
@@ -1,14 +1,14 @@
 package com.springboot.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.project.entity.Employee;
 import com.springboot.project.repository.EmployeeRepository;
 
-@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
+//@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
 @RestController
 public class EmployeeController {
 
